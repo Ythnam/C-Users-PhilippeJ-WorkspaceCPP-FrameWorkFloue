2b22aa53f5b751f15884fd81d3cd64a6a1753c8d
kangoux <le.boss30@live.fr>
2016-02-24 16:49:43 +0100

    WiP valueModel works!

--- FrameWorkFloue/ValueModel.h
+++ FrameWorkFloue/ValueModel.h
@@ -1,48 +1,54 @@
 /*
  * ValueModel.h
  *
- *  Created on: 2 févr. 2016
- *      Author: PhilippeJ
+ *  Created on: 9 févr. 2016
+ *      Author: Mathieu
  */
+#ifndef VALUEMODEL_H
+#define VALUEMODEL_H
 
-#ifndef VALUEMODEL_H_
-#define VALUEMODEL_H_
 #include "Expression.h"
 
-namespace core{
+
+namespace core
+{
+
 template <class T>
-class ValueModel : public Expression<T>{
+class ValueModel : public Expression<T>
+{
+public :
+	ValueModel();
+	ValueModel(const T&);
+	T evaluate() const;
+	void setValue(const T&);
 
-public : ValueModel();
-		 ValueModel(const T&);
-		 virtual ~ValueModel(){};
-
-		 virtual T evaluate() const;
-		 virtual void setValue(const T&);
-
-private : T value;
-
+private:
+	T value;
 };
 
-template <class T>
-ValueModel<T>::valueModel():value(0){
 
+template <class T>
+ValueModel<T>::ValueModel():
+value(T(0))
+{
 }
 
 template <class T>
-ValueModel<T>::valueModel(const T& _value):value(_value){
-
+ValueModel<T>::ValueModel(const T& _value):
+value(_value)
+{
 }
 
 template <class T>
-void ValueModel<T>::setValue(const T& _value){
-	value=_value;
-}
-
-template <class T>
-T ValueModel<T>::evaluate() const{
+T ValueModel<T>::evaluate() const
+{
 	return value;
 }
 
+template <class T>
+void ValueModel<T>::setValue(const T& _value)
+{
+	value=_value;
 }
-#endif /* VALUEMODEL_H_ */
+}
+#endif
