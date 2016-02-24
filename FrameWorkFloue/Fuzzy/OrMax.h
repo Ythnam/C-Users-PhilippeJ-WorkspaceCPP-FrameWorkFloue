f752a41ee0ad1089e0d093d9c3dc746e06c30943
Ythnam <philidesalondepce@hotmail.fr>
2016-02-24 17:54:12 +0100

    méthode évaluate de OrMax fait

--- FrameWorkFloue/Fuzzy/OrMax.h
+++ FrameWorkFloue/Fuzzy/OrMax.h
@@ -16,5 +16,13 @@ class OrMax: public fuzzy::Or{
 public : virtual T evaluate(core::Expression<T>*, core::Expression<T>*) const;
 
 };
+
+template<class T>
+T OrMax<T>::evaluate(core::Expression<T>* left, core::Expression<T>* right) const{
+	T l = left->evaluate();
+	T r = right->evaluate();
+	return (l >= r) ? l : r;
+}
+
 }
 #endif /* ORMAX_H_ */
