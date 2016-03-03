1fd7f2d2095e1d26ebe4a64a423cc94b654566ed
Ythnam <philidesalondepce@hotmail.fr>
2016-03-03 11:47:02 +0100

    Class Evaluator -> reprendre la fonction buildShape

--- FrameWorkFloue/Fuzzy/MamdaniDefuzz.h
+++ FrameWorkFloue/Fuzzy/MamdaniDefuzz.h
@@ -9,15 +9,20 @@
 #define MAMDANIDEFUZZ_H_
 
 #include "Core/BinaryExpression.h"
+#include "Fuzzy/Evaluator.h"
 
 namespace fuzzy{
 template <class T>
 class MamdaniDefuzz : public core::BinaryExpression{
+
 public : virtual T evaluate(core::Expression<T>*, core::Expression<T>*);
-virtual T defuzz(Shape) const = 0;
+		 virtual T defuzz(Shape) const = 0;
+
 };
+
 template <class T>
 T MamdaniDefuzz<T>::evaluate(core::Expression<T>* outPutVar, core::Expression<T>* fuzzySystem){
+	return defuzz(Evaluator<T>::BuildShape(outPutVar, fuzzySystem);
 }
 }
 #endif /* MAMDANIDEFUZZ_H_ */
